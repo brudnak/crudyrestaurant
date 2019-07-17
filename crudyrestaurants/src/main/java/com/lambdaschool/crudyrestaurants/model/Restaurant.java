02a5c528ec166648a0416ca39fc7203496201bb7
Andrew Brudnak <brudnak@icloud.com>
2019-07-17 02:12:51 -0700

    :apple: Restaurant class work

--- crudyrestaurants/src/main/java/com/lambdaschool/crudyrestaurants/model/Restaurant.java
+++ crudyrestaurants/src/main/java/com/lambdaschool/crudyrestaurants/model/Restaurant.java
@@ -23,4 +23,72 @@ public class Restaurant {
             cascade = CascadeType.ALL,
             orphanRemoval = true)
     private ArrayList<Menu> menus = new ArrayList<>();
+
+    public Restaurant() {
+    }
+
+    public Restaurant(String name, String address, String city, String state, String telephone) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.telephone = telephone;
+    }
+
+    public long getRestaurantid() {
+        return restaurantid;
+    }
+
+//    public void setRestaurantid(long restaurantid) {
+//        this.restaurantid = restaurantid;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public ArrayList<Menu> getMenus() {
+        return menus;
+    }
+
+    public void setMenus(ArrayList<Menu> menus) {
+        this.menus = menus;
+    }
 }
+
