c2f8f96ea2efe1772f8bdf02db0e23a5335607e2
Andrew Brudnak <brudnak@icloud.com>
2019-07-17 02:14:37 -0700

    :hammer: Menu model getting set up

--- crudyrestaurants/src/main/java/com/lambdaschool/crudyrestaurants/model/Menu.java
+++ crudyrestaurants/src/main/java/com/lambdaschool/crudyrestaurants/model/Menu.java
@@ -17,4 +17,45 @@ public class Menu {
     @ManyToOne
     @JoinColumn(name = "restaurantid", nullable = false)
     private Restaurant restaurant;
+
+    public Menu() {
+    }
+
+    public Menu(String dish, double price, Restaurant restaurant) {
+        this.dish = dish;
+        this.price = price;
+        this.restaurant = restaurant;
+    }
+
+    public long getMenuid() {
+        return menuid;
+    }
+
+//    public void setMenuid(long menuid) {
+//        this.menuid = menuid;
+//    }
+
+    public String getDish() {
+        return dish;
+    }
+
+    public void setDish(String dish) {
+        this.dish = dish;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
 }
