45a61a0f727687ced4bfae9ae7fdf33388a5051d
Andrew Brudnak <brudnak@icloud.com>
2019-07-17 01:45:38 -0700

    :bento: Getting restaurant class set up

--- crudyrestaurants/src/main/java/com/lambdaschool/crudyrestaurants/model/Restaurant.java
+++ crudyrestaurants/src/main/java/com/lambdaschool/crudyrestaurants/model/Restaurant.java
@@ -1,4 +1,23 @@
 package com.lambdaschool.crudyrestaurants.model;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "restaurant")
 public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long restaurantid;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    private String address;
+    private String city;
+    private String state;
+    private String telephone;
+
+    private ArrayList<Menu> menus = new ArrayList<>();
 }
