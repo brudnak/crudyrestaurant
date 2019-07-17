273ff590d0cf075e85f8981e2fe3524c84dd11ec
Andrew Brudnak <brudnak@icloud.com>
2019-07-17 01:52:21 -0700

    :newspaper: Menu getting set up

--- crudyrestaurants/src/main/java/com/lambdaschool/crudyrestaurants/model/Menu.java
+++ crudyrestaurants/src/main/java/com/lambdaschool/crudyrestaurants/model/Menu.java
@@ -1,4 +1,20 @@
 package com.lambdaschool.crudyrestaurants.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "menu")
 public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long menuid;
+
+    @Column(nullable = false)
+    private String dish;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurantid", nullable = false)
+    private Restaurant restaurant;
 }
